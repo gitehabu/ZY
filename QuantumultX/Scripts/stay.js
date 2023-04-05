356a1307264505d4268593bb74b9788d0b60c3c6
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-05 17:17:07 +0800

    Create stay.js

---
+++ QuantumultX/Scripts/stay.js
@@ -0,0 +1,13 @@
+// @https://github.com/ddgksf2013/Scripts/blob/master/stay.js
+// ScriptName  stay
+// 2023-04-05  15:30
+
+
+/* ================ ⭐️⭐️ ================ */
+// 2022-10-17
+
+let ZY = JSON.parse($response.body);
+if (ZY.biz) {
+    ZY.biz = Object.values(ZY.biz).filter(item => !(item["type"]=="promoted"));
+}
+$done({ body: JSON.stringify(ZY) });
