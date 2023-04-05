a3631cd1827f7233f8ebf71e12b267104cd41637
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-05 15:32:06 +0800

    Create baidu-index.js

---
+++ QuantumultX/Scripts/baidu-index.js
@@ -0,0 +1,15 @@
+// @https://github.com/RuCu6/QuanX/tree/main/Scripts/baidu
+// ScriptName   baidu-index
+// 2023-04-05 15:30
+
+
+/* ================ ⭐️⭐️ ================ */
+//百度搜索首页净化
+//Author  @Adblock4limbo, @RuCu6
+//右上角入口按钮 .square-enterance,
+
+let rHead = "<head>";
+let newStyle =
+  "<head><style> #foot, .recordcode, .index-copyright, div[style*=overflow], article, .rn-container, .ns-square-point, .voice.call, .ts-image-uploader, .ts-image-uploader-icon, .ts-image-uploader-icon-point, .qrcode.call, .navs-bottom-bar, .tab_news_1, .s-loading-frame.bottom{display:none!important} </style>";
+let body = $response.body.replace(rHead, newStyle);
+$done({ body });
