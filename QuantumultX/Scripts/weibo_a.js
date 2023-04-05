14cd201c63dbe2851c46181bac304896b74d27a7
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-05 16:17:46 +0800

    更新 weibo_a.js

--- QuantumultX/Scripts/weibo_a.js
+++ QuantumultX/Scripts/weibo_a.js
@@ -1,7 +1,9 @@
 // @https://github.com/RuCu6/QuanX/blob/main/Scripts/weibo.js
-// 2023-04-05 15:15
+// ScriptName  weibo
+// 2023-04-05  15:30
 
 // 注释掉铁粉标识,如下 /* */
+
 // 头像挂件,关注按钮.(评论区铁粉标识)
 /* removeAvatar(item.data); */
 
