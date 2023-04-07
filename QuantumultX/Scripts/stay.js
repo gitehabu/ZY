afcbbe27b078e0d784f58a3b61a0315e3fe8b9c4
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-08 00:26:28 +0800

    更新 stay.js

--- QuantumultX/Scripts/stay.js
+++ QuantumultX/Scripts/stay.js
@@ -1,9 +1,7 @@
 // https://github.com/ddgksf2013/Scripts/blob/master/stay.js
 // ScriptName stay
 
-/* ================ ⭐️⭐️ ================ */
 // 2022-10-17
-
 let ZY = JSON.parse($response.body);
 if (ZY.biz) {
     ZY.biz = Object.values(ZY.biz).filter(item => !(item["type"]=="promoted"));
