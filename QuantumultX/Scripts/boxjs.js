f4cce4ef6187e99c9fe8671be103eedf83656fa0
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-08 00:05:37 +0800

    Update boxjs.js

--- QuantumultX/Scripts/boxjs.js
+++ QuantumultX/Scripts/boxjs.js
@@ -1,4 +1,6 @@
-// @https://github.com/chavyleung/scripts/blob/master/box/chavy.boxjs.js
+// https://github.com/chavyleung/scripts/blob/master/box/chavy.boxjs.js
+// hostname = boxjs.com, boxjs.net, *.boxjs.com, *.boxjs.net
+// ^https?:\/\/(.+\.)?boxjs\.(com|net) url script-analyze-echo-response https://raw.githubusercontent.com/chavyleung/scripts/master/box/chavy.boxjs.js
 
 /* ================ ⭐️⭐️ ================ */
 const $ = new Env('BoxJs')
