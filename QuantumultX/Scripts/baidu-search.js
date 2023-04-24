6b7afaa65863a0cd2cf41a0cf4e94a3a5d20644c
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-24 10:14:09 +0800

    Update and rename Baidu-search.js to baidu-search.js

--- QuantumultX/Scripts/baidu-search.js
+++ QuantumultX/Scripts/baidu-search.js
@@ -1,7 +1,7 @@
 /*
 https://github.com/RuCu6/QuanX/blob/main/Scripts/webpage/baidu.js
 百度_搜索结果广告 //www.baidu.com, m.baidu.com
-^https?:\/\/(www|m)\.baidu\.com\/.*word=.* url script-response-body https://raw.githubusercontent.com/ZY714IU/ZY/main/QuantumultX/Scripts/Baidu-search.js
+^https?:\/\/(www|m)\.baidu\.com\/.*word=.* url script-response-body https://raw.githubusercontent.com/ZY714IU/ZY/main/QuantumultX/Scripts/baidu-search.js
 */
 
 
