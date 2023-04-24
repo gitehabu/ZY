a8f592387b5fb658ae2442bc371e1356d5122f9f
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-24 10:06:01 +0800

    Create baidu-zhidao.js

---
+++ QuantumultX/Scripts/baidu-zhidao.js
@@ -0,0 +1,12 @@
+/*
+https://github.com/RuCu6/QuanX/blob/main/Scripts/webpage/baidu-zhidao.js
+百度_知道搜索广告 //zhidao.baidu.com
+^https?:\/\/zhidao\.baidu\.com\/(question|index|\?fr|\?word) url script-response-body
+*/
+
+
+let rHead = "<head>";
+let newStyle =
+  "<head><style> .ec_ad_results, .ad-icon, .wpbyuwfarr-ecom-ads, div[class*=fc-][tplid], .w-question-list[data-sign], .ec-ad{display:none!important} </style>";
+let body = $response.body.replace(rHead, newStyle);
+$done({ body });
