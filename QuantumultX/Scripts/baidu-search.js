2ff695954418f67bd8d7ad04d1af200ad589b6bd
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-24 18:49:55 +0800

    更新 baidu-search.js

--- QuantumultX/Scripts/baidu-search.js
+++ QuantumultX/Scripts/baidu-search.js
@@ -1,6 +1,6 @@
 /*
 https://github.com/RuCu6/QuanX/blob/main/Scripts/webpage/baidu.js
-百度_搜索结果广告 //www.baidu.com, m.baidu.com
+ScriptName 百度_搜索结果广告
 ^https?:\/\/(www|m)\.baidu\.com\/.*word=.* url script-response-body https://raw.githubusercontent.com/ZY714IU/ZY/main/QuantumultX/Scripts/baidu-search.js
 */
 
