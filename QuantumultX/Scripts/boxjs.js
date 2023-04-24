2675424f20f6b27cc98d9cc4cd6443112693a1e5
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-24 19:08:18 +0800

    更新 boxjs.js

--- QuantumultX/Scripts/boxjs.js
+++ QuantumultX/Scripts/boxjs.js
@@ -1,8 +1,8 @@
-/*
+/********
 https://github.com/chavyleung/scripts/blob/master/box/chavy.boxjs.js
 hostname = boxjs.com, boxjs.net, *.boxjs.com, *.boxjs.net
 ^https?:\/\/(.+\.)?boxjs\.(com|net) url script-analyze-echo-response https://raw.githubusercontent.com/chavyleung/scripts/master/box/chavy.boxjs.js
-*******/
+********/
 
 
 const $ = new Env('BoxJs')
