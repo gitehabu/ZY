78667d400718a8dc210121542e8e9d262a56ae01
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-24 09:57:16 +0800

    Update baidu-no-redirect.js

--- QuantumultX/Scripts/baidu-no-redirect.js
+++ QuantumultX/Scripts/baidu-no-redirect.js
@@ -1,6 +1,7 @@
 /* 
 百度搜索防跳转AppStore
 https://github.com/app2smile/rules/blob/master/js/baidu-no-redirect.js
+^https?:\/\/boxer\.baidu\.com\/scheme\?scheme url script-response-header https://raw.githubusercontent.com/ZY714IU/ZY/main/QuantumultX/Scripts/baidu-no-redirect.js
 */
 
 
