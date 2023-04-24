66f22e27c1041abb0ae44bc0561166990d5946c1
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-24 19:17:46 +0800

    更新 xiaohongshu_a.js

--- QuantumultX/Scripts/xiaohongshu_a.js
+++ QuantumultX/Scripts/xiaohongshu_a.js
@@ -1,6 +1,9 @@
-// https://github.com/RuCu6/QuanX/blob/main/Scripts/xiaohongshu.js
-// ScriptName xiaohongshu
-// 2023-03-04 11:10
+/********
+https://github.com/RuCu6/QuanX/blob/main/Scripts/xiaohongshu.js
+ScriptName xiaohongshu
+2023-03-04 11:10
+********/
+
 
 const url = $request.url;
 if (!$response.body) $done({});
