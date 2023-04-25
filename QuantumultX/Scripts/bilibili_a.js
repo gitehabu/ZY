0bc3c0ec58229b21bc151cf4a4be9bfe566a094f
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-25 10:08:49 +0800

    更新 bilibili_a.js

--- QuantumultX/Scripts/bilibili_a.js
+++ QuantumultX/Scripts/bilibili_a.js
@@ -1,7 +1,13 @@
 /********
-https://github.com/RuCu6/QuanX/blob/main/Scripts/bilibili/bili.js
-ScriptName bilibili
-2023-04-01 08:55
+https://github.com/RuCu6/QuanX/blob/9a67ac7a83380a7c1cfaa23a3cfe7ac42a7a9ece/Scripts/bilibili/bili.js
+2023-04-25 08:28
+
+更改以下
+          item.name === "直播" ||
+          item.name === "推荐" ||
+          item.name === "热门" ||
+          item.name === "数码" ||
+          item.name === "影视"
 ********/
 
 
@@ -21,9 +27,10 @@ if (!$response.body) {
     if (obj.data.tab) {
       obj.data.tab = obj.data.tab.filter(
         (item) =>
+          item.name === "直播" ||
           item.name === "推荐" ||
           item.name === "热门" ||
-          item.name === "动画" ||
+          item.name === "热门" ||
           item.name === "影视"
       );
       fixPos(obj.data.tab);
@@ -119,7 +126,7 @@ if (!$response.body) {
       obj.data.vip.due_date = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
       obj.data.vip.role = 3;
     }
-  } else if (url.includes("/x/v2/feed/index")) {
+  } else if (url.includes("/x/v2/feed/index?")) {
     // 推荐广告
     if (obj.data?.items) {
       obj.data.items = obj.data.items.filter((i) => {
@@ -136,15 +143,21 @@ if (!$response.body) {
             // return false;
             // }
             return false;
-            // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告 ad_inline_3d 上方大的视频3d广告
+            // ad_av 创作推广广告
+            // ad_inline_3d 上方大的视频3d广告
+            // ad_inline_eggs 上方大的视频广告
+            // ad_player 大视频广告
+            // ad_web_gif 大gif广告
+            // ad_web_s 普通小广告
           } else if (
             cardType === "cm_v2" &&
             [
-              "ad_web_s",
               "ad_av",
-              "ad_web_gif",
+              "ad_inline_3d",
+              "ad_inline_eggs",
               "ad_player",
-              "ad_inline_3d"
+              "ad_web_gif",
+              "ad_web_s"
             ].includes(cardGoto)
           ) {
             return false;
@@ -161,6 +174,15 @@ if (!$response.body) {
         }
         return true;
       });
+    }
+  } else if (url.includes("/x/v2/feed/index/story")) {
+    if (obj.data?.items) {
+      obj.data.items = obj.data.items.filter(
+        (i) => !(
+          i.hasOwnProperty("ad_info") ||
+          i.card_goto.includes("ad")
+        )
+      );
     }
   } else if (url.includes("/x/v2/search/square")) {
     // 热搜广告
