278b1d6dd1065c3ed2da39fdadd2cb813ffa03b0
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-25 15:44:29 +0800

    更新 bilibili_a.js

--- QuantumultX/Scripts/bilibili_a.js
+++ QuantumultX/Scripts/bilibili_a.js
@@ -1,7 +1,6 @@
 /********
 https://github.com/RuCu6/QuanX/blob/9a67ac7a83380a7c1cfaa23a3cfe7ac42a7a9ece/Scripts/bilibili/bili.js
 2023-04-25 08:28
-
 tab 更改为以下
           item.name === "直播" ||
           item.name === "推荐" ||
