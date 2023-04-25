8de86198f4d4d7e439bf8e953b422af65a6da337
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-25 15:33:52 +0800

    更新 baidu-zhidao.js

--- QuantumultX/Scripts/baidu-zhidao.js
+++ QuantumultX/Scripts/baidu-zhidao.js
@@ -4,7 +4,6 @@ ScriptName 百度_知道搜索广告
 ^https?:\/\/zhidao\.baidu\.com\/(question|index|\?fr|\?word) url script-response-body
 ********/
 
-
 let rHead = "<head>";
 let newStyle =
   "<head><style> .ec_ad_results, .ad-icon, .wpbyuwfarr-ecom-ads, div[class*=fc-][tplid], .w-question-list[data-sign], .ec-ad{display:none!important} </style>";
