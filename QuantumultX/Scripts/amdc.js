269a117f6f76a1adad4b8ce35729a7811352cabd
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-25 23:30:17 +0800

    更新 amdc.js

--- QuantumultX/Scripts/amdc.js
+++ QuantumultX/Scripts/amdc.js
@@ -1,8 +1,8 @@
-/********
+/*================
 https://github.com/RuCu6/QuanX/blob/main/Scripts/amdc.js
 2023-03-31 17:20
 ^http[s]?:\/\/amdc\.m\.taobao\.com\/amdc\/mobileDispatch$ url script-response-header https://raw.githubusercontent.com/ZY714IU/ZY/main/QuantumultX/Scripts/amdc.js
-********/
+================*/
 
 const url = $request.url;
 const header = $request.headers;
