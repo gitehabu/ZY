1190e1c4529cb5348e434dd1ad1bb3e63e97705f
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-25 15:33:08 +0800

    更新 amdc.js

--- QuantumultX/Scripts/amdc.js
+++ QuantumultX/Scripts/amdc.js
@@ -4,16 +4,13 @@ https://github.com/RuCu6/QuanX/blob/main/Scripts/amdc.js
 ^http[s]?:\/\/amdc\.m\.taobao\.com\/amdc\/mobileDispatch$ url script-response-header https://raw.githubusercontent.com/ZY714IU/ZY/main/QuantumultX/Scripts/amdc.js
 ********/
 
-
 const url = $request.url;
 const header = $request.headers;
 let ua = header["User-Agent"] || header["user-agent"];
-
 if (url.includes("/amdc/mobileDispatch")) {
   if (ua.includes("AMapiPhone") || ua.includes("Cainiao")) {
     $done({ status: "HTTP/1.1 404 Not Found" });
     return;
   }
 }
-
 $done({});
