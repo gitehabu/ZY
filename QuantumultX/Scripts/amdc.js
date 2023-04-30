7dbc60d9353d163fcfe9f7486cc2beaf789b9462
YU <7240762+ZY714IU@users.noreply.github.com>
2023-04-30 21:42:22 +0800

    更新 amdc.js

--- QuantumultX/Scripts/amdc.js
+++ QuantumultX/Scripts/amdc.js
@@ -14,3 +14,16 @@ if (url.includes("/amdc/mobileDispatch")) {
   }
 }
 $done({});
+
+
+
+/*================
+
+
+
+
+
+const version = 'V1.0.4';
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];-1!=ua.indexOf("AMap")||-1!=ua.indexOf("Cainiao")||-1!=ua.indexOf("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C")?$done({body:"ddgksf2013"}):$done({});
+
+================*/
