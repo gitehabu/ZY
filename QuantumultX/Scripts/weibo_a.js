18d3a2f5f938e603c046c931af10f0dcc61c2daf
YU <7240762+ZY714IU@users.noreply.github.com>
2023-05-01 11:09:47 +0800

    Update weibo_a.js

--- QuantumultX/Scripts/weibo_a.js
+++ QuantumultX/Scripts/weibo_a.js
@@ -664,11 +664,11 @@ if (url.includes("/interface/sdk/sdkad.php")) {
       delete obj.button_configs;
     }
     // 广场页
- /* if (obj.channelInfo.channel_list) {
+   if (obj.channelInfo.channel_list) {
       obj.channelInfo.channel_list = obj.channelInfo.channel_list.filter(
         (t) => t.title !== "广场"
       );
-    } */
+    }
   } else if (url.includes("/v1/ad/preload")) {
     // 开屏广告
     if (obj.ads) {
